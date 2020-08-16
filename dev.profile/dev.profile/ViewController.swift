7cf0ff6f6c115c6f5017b4268b77e228a4201f4b
AnelMengdigali <54207471+AnelMengdigali@users.noreply.github.com>
2020-08-16 22:25:47 +0600

    Add files via upload

---
+++ dev.profile/dev.profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev.profile
+//
+//  Created by ggg on 09.07.2020.
+//  Copyright © 2020 anelapplab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
